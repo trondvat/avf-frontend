75f77d15b32626f9c09d79a0a90148b854b96554
trymtv <tiltrym@gmail.com>
2021-04-24 20:29:45 +0200

    Add lint rules

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  settings: {
+    react: {
+      version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+  extends: ["airbnb-typescript"],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    "react/function-component-definition": [
+      "warn",
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
+    quotes: [
+      "warn",
+      "double",
+      { avoidEscape: true, allowTemplateLiterals: true },
+    ],
+    "linebreak-style": "off",
+    "@typescript-eslint/quotes": "off",
+    "arrow-body-style": "off",
+  },
+};
