d41354cb5f6822dacc2a9922a2aa8bfd7f51f79c
trymtv <tiltrym@gmail.com>
2021-04-26 14:48:55 +0200

    Add the default layout scheme
    There are also defined some extra tailwind fonts and colors.

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = (): JSX.Element => {
+  return <div className="h-24 bg-avf-red opacity-80" />;
+};
+
+export default Footer;
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "assets/logo.svg";
+
+const Header = (): JSX.Element => {
+  return (
+    <div>
+      <nav className="flex items-center justify-between flex-wrap bg-avf-black p-6 px-28 h-24">
+        <img src={logo} className=" max-h-full max-w-full" alt="Logo" />
+      </nav>
+      <hr className=" mx-28 border-avf-red border-2 rounded-sm" />
+    </div>
+  );
+};
+
+export default Header;
